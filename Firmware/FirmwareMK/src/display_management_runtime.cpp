7c88b6851180d9b5b854bb98bdf760e188b59db2
William Redenbaugh <wredenba@amazon.com>
2022-09-17 21:30:48 -0700

    added i2c lcd test code... needss more work and refractoring

--- Firmware/FirmwareMK/src/display_management_runtime.cpp
+++ Firmware/FirmwareMK/src/display_management_runtime.cpp
@@ -1,9 +1,12 @@
 #include "display_management_runtime.h"
 #include "STM32FreeRTOS.h"
 #include "mk_helper.h"
+#include "i2c_lcd.h"
+
 void display_management_thread(void *params){
     (void)params;
 
+    init_lcd(20, 4);
     for(;;){
         MK_TASK_DELAY_MS(100);
     }
